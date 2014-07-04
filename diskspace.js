d690faad01fdf76f001d85e92fb8a5e67eebaadc
Sergey <sv0lapin@gmail.com>
2014-07-04 15:44:17 +0400

    bring node.js callback style (err as a firtst parameter) can be helpful for #2

--- diskspace.js
+++ diskspace.js
@@ -1,6 +1,7 @@
 'use strict';
 var os = require('os');
 var child_process = require('child_process');
+var path = require('path');
 
 function check(drive, callback)
 {
@@ -11,18 +12,22 @@ function check(drive, callback)
 	if (!drive)
 	{
 		status = 'NOTFOUND';
-		return callback(total, free, status);
+		var error = new Error('Necessary parameter absent');
+
+		return callback
+					? callback(error, total, free, status)
+					: console.error(error);
 	}
 
 	if (os.type() == 'Windows_NT') //Windows
 	{
-		child_process.exec('"' + __dirname + '\\drivespace.exe" drive-' + drive, function(error, stdout, stderr)
+		child_process.exec(path.join(__dirname, 'drivespace.exe') + ' drive-' + drive, function(error, stdout, stderr)
 		{
 			if (error)
 			{
 				status = 'STDERR';
-				console.log(stderr);
-				callback(total, free, status);
+				callback ? callback(error, total, free, status)
+						 : console.error(stderr);
 			}
 			else
 			{
@@ -32,7 +37,7 @@ function check(drive, callback)
 				free = disk_info[1];
 				status = disk_info[2];
 
-				callback(total, free, status);
+				callback && callback(null, total, free, status);
 			}
 		});
 	}
@@ -45,14 +50,13 @@ function check(drive, callback)
 				if (stderr.indexOf("No such file or directory") != -1)
 				{
 					status = 'NOTFOUND';
-					callback(total, free, status);
 				}
 				else
 				{
 					status = 'STDERR';
-					console.log(stderr);
-					callback(total, free, status);
 				}
+				callback ? callback(error, total, free, status)
+						 : console.error(stderr);
 			}
 			else
 			{
@@ -65,7 +69,7 @@ function check(drive, callback)
 				free = disk_info[3] * 1024;
 				status = 'READY';
 
-				callback(total, free, status);
+				callback && callback(null, total, free, status);
 			}
 		});
 	}
